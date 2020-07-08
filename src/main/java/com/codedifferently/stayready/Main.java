a398980ff0243a1dc0e1ecafdc2eac13fe4e1374
Paola <camachopaola311@gmail.com>
2020-07-08 16:14:17 -0400

    Finished StayReadyLab5

--- src/main/java/com/codedifferently/stayready/Main.java
+++ src/main/java/com/codedifferently/stayready/Main.java
@@ -1,4 +1,59 @@
 package com.codedifferently.stayready;
 
 public class Main {
+
+    public String dollarToEuro(double dollarAmount){
+        double mult = dollarAmount * 0.8861;
+        return String.format("€%.2f", (mult));
+    }
+
+    public String euroToDollar(double euroAmount){
+        double mult = (euroAmount*1.1286);
+        return String.format("$%.2f", (mult));
+    }
+
+    public String euroToPound(double euroAmount){
+        double mult = euroAmount * 0.89923;
+        return String.format("£%.2f", (mult));
+    }
+
+    public String poundToRupee(double poundAmount){
+        double mult = poundAmount * 94.13;
+        return String.format("₹%.2f", (mult));
+    }
+
+    public String rupeeToCanadian(double rupeeAmount){
+        double mult = rupeeAmount * 0.0181;
+        return String.format("C$%.2f", (mult));
+    }
+
+    public String canadianToSingapore(double canadianAmount){
+        double mult = canadianAmount * 1.028;
+        return String.format("SGD%.2f", (mult));
+    }
+
+    public String singaporeToFranc(double singaporeAmount){
+        double mult = singaporeAmount * 0.677;
+        return String.format("SFr%.2f", (mult));
+    }
+
+    public String francToRinggit(double francAmount){
+        double mult = francAmount * 4.536;
+        return String.format("RM%.2f", (mult));
+        
+    }
+
+    public String ringgitToYen(double ringgitAmount){
+        double mult = ringgitAmount *25.09;
+        return String.format("¥%.2f", (mult));
+        
+    }
+
+    public String yenToYuan(double yenAmount){
+        double mult = yenAmount * 0.0656;
+        return String.format("¥%.2f", (mult));
+    }
+    
+    //how much you have multiplied by exchange rate
 }
+
--- src/test/java/com/codedifferently/stayready/MainTest.java
+++ src/test/java/com/codedifferently/stayready/MainTest.java
@@ -1,4 +1,132 @@
 package com.codedifferently.stayready;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
 
 public class MainTest {
+    Main main;
+
+    @Before
+    public void mainObject(){
+        main = new Main();
+    }
+
+    @Test
+    public void DollarToEuroTest(){
+        //Given
+        String expected = "€17.72";
+
+        //When
+        String actual = main.dollarToEuro(20);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void EuroToDollarTest(){
+        //Given
+        String expected = "$22.57";
+
+        //When
+        String actual = main.euroToDollar(20);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void EuroToPound(){
+        //Given
+        String expected = "£17.98";
+
+        //When
+        String actual = main.euroToPound(20);
+        
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void PoundToRupee(){
+        //Given
+        String expected = "₹470.65";
+
+        //When
+        String actual = main.poundToRupee(5);
+        
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+     @Test
+    public void RupeetoCanadianTest(){
+        //Given
+        String expected = "C$1.81";
+
+        //When
+        String actual = main.rupeeToCanadian(100);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    } 
+
+    
+    @Test
+    public void CanadiantoSingaporeTest(){
+        //Given
+        String expected = "SGD20.56";
+
+        //When
+        String actual = main.canadianToSingapore(20);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void SingaporetoFranc(){
+         //Given
+        String expected = "SFr13.54";
+
+        //When
+        String actual = main.singaporeToFranc(20);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void FranctoRinggit(){
+        //Given
+        String expected = "RM90.72";
+
+        //When
+        String actual = main.francToRinggit(20);
+
+        //Then
+        Assert.assertEquals(expected, actual);      
+    }
+
+    @Test
+    public void RinggittoYen(){
+        //Given
+        String expected = "¥125.45";
+
+        //When
+        String actual = main.ringgitToYen(5);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void YentoYuan(){
+        //Given
+        String expected = "¥3.28";
+
+        //When
+        String actual = main.yenToYuan(50);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+ 
 }
